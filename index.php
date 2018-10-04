f6db965553e1da8f334758277d3277234841dbd7
jklimes12 <jklimes12@seznam.cz>
2018-10-04 15:11:09 +0200

    tanga

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$gender = "";
+
+switch ($gender) {
+    case "male":
+        echo "pokud nejsi gay tak nenos tanga";
+        break;
+    case "female":
+        echo "pokud jsi pěkná nos tanga";
+        break;
+    default:
+        echo "nevím co jsi ";
+}
+?>
